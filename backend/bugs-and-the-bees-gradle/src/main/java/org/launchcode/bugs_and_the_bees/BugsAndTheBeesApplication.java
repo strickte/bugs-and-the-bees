89a383d951dd49743b72211fab5f42cee252d755
strickte <166240135+strickte@users.noreply.github.com>
2025-05-01 21:07:47 -0500

    basic file tree structure with gitignore, license, and Gradle

---
+++ backend/bugs-and-the-bees-gradle/src/main/java/org/launchcode/bugs_and_the_bees/BugsAndTheBeesApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.bugs_and_the_bees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugsAndTheBeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugsAndTheBeesApplication.class, args);
+	}
+
+}
---
+++ backend/bugs-and-the-bees-gradle/src/test/java/org/launchcode/bugs_and_the_bees/BugsAndTheBeesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.bugs_and_the_bees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugsAndTheBeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
