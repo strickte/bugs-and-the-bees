83ddd48cb915c51d5eee6d9181f2df0ce6d5fbca
strickte <166240135+strickte@users.noreply.github.com>
2025-05-04 14:19:07 -0500

    added Tailwind, but not displaying

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import {
+  Routes,
+  Route,
+  useNavigate,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-yellow-500">Hello, World! Testing Tailwind</h1>
+    </>
+  );
+}
+
+export default App;
